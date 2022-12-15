c876f908361bd1769e993b26e9b68af647df0b35
blycho <pablo.lichtig@gmail.com>
2022-12-15 18:42:57 -0300

    added reg events

--- pmfMat_analysis.py
+++ pmfMat_analysis.py
@@ -31,6 +31,9 @@ unc = pd.read_excel('PMF_BA_full.xlsx', decimal=',', sheet_name='UNC', index_col
 unc = unc.rename(columns={'PM2,5': 'PM2.5'})
 unc[unc < 0] = np.nan
 unc = unc.reindex(sorted(unc.columns), axis=1)
+print(matrix.keys())
+
+events = pd.read_excel('BA_events.xlsx', index_col='date')
 
 # -
 
@@ -53,8 +56,52 @@ ax.errorbar(matrix.index, mass[1]['elemental_C'], yerr=mass[3]['uelemental_C'], 
 ax.errorbar(matrix.index, mass[1]['salt'], yerr=mass[3]['usalt'], marker='.', capsize=3, capthick=1, linestyle='-', label="Sea salt")
 
 ax.set_title('Time series')
+ax.set_xlabel("Date")
+ax.set_ylabel("Mass concentration (µg/m$^3$)")
 ax.legend()
 plt.show()
+# -
+
+
+
+# +
+# %matplotlib widget
+
+methods = ['Solomon_1989', 'Chow_1994', 'Malm_1994', 'Chow_1996', 'Andrews_2000',
+           'Malm_2000', 'Maenhaut_2002', 'DeBell_2006', 'Hand_2011']
+
+#methods = ['Hand_2011']
+
+d_methodQuality = {}
+
+
+fig, axs = plt.subplots(3, 3, figsize=(20, 15))
+
+i, j = 0, 0
+for method in methods:
+    d_methodQuality[method] = 0
+    mass = mass_reconstruction(matrix, unc, equation=method)
+    reconst = mass[0]/matrix['PM2.5']
+    ureconst = np.sqrt((1/matrix['PM2.5'] * unc['PM2.5'])**2 + (matrix['PM2.5']/mass[0]/mass[0] * mass[2])**2)
+    axs[i][j].errorbar(matrix.index, reconst,  yerr=ureconst, label=method, capsize=2, capthick=1)
+    #axs[i][j].plot(matrix.index, reconst.where(np.logical_or(events['Event']=='S', events['Event']=='SP', events['Event']=='SN')), 'ro')
+    axs[i][j].plot(matrix.index, reconst.where(events['Event']=='S'), 'ro', label='Smoke')
+    axs[i][j].plot(matrix.index, reconst.where(events['Event']=='SP'), 'go', label='Smoke previous day')
+    axs[i][j].plot(matrix.index, reconst.where(events['Event']=='SN'), 'ko', label='Smoke next day')
+    axs[i][j].set_title(method)
+    axs[i][j].legend()
+    axs[i][j].axhline(0.8, color='k')
+    axs[i][j].axhline(1.2, color='k')
+    axs[i][j].tick_params(labelrotation=45)
+    j += 1
+    if j%3==0:
+        j = 0
+        i += 1
+    d_methodQuality[method] = np.logical_and(((reconst + ureconst) > 0.8), ((reconst - ureconst) < 1.2)).sum()
+    
+plt.show()
+    
+
 
 # +
 #Prepare correlation plots
