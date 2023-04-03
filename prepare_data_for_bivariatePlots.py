97c4873ad3839dd34694f93eb9a29543709a41e4
blycho <pablo.lichtig@gmail.com>
2023-04-03 15:56:18 -0300

    Create data with ws and wd

--- prepare_data_for_bivariatePlots.py
+++ prepare_data_for_bivariatePlots.py
@@ -18,19 +18,25 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+from parserSFC import readSFC
 
 data = pd.read_excel('PMF_BA_full.xlsx')
 data['date'] = pd.to_datetime(data['date'])+ dt.timedelta(hours=12)
 data.set_index(data['date'], inplace=True)
 data.drop('date', axis=1, inplace=True)
 
+meteo = readSFC('OBSERVATORIO.SFC')
+
 orig_hours = data.index
 
-print(orig_hours + dt.timedelta(hours=23))
+
 for hour in orig_hours:
     for extended in range(1, 24):
         data.loc[hour + dt.timedelta(hours=extended)] = data.loc[hour]
 data.sort_index(inplace=True)
+meteo = meteo.loc[list(data.index)]
+
+data = pd.concat([data, meteo[['ws', 'wd']]], axis=1)
 data.to_csv('data_every_hour_duplicate.csv')
 
 
