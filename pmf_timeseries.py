0403b465392e5427787b609d528ac005c23ffcd7
blycho <pablo.lichtig@gmail.com>
2023-05-08 17:11:09 -0300

    Add boxplots

--- pmf_timeseries.py
+++ pmf_timeseries.py
@@ -18,9 +18,10 @@
 import numpy as np
 import datetime as dt
 import pandas as pd
-import altair as alt
+#import altair as alt
 from load_data import load_data
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 matrix, unc, meteo, gases, events = load_data('PMF_BA_full.xlsx', 'PMF_BA_full.xlsx',
@@ -28,12 +29,49 @@ matrix, unc, meteo, gases, events = load_data('PMF_BA_full.xlsx', 'PMF_BA_full.x
                                                'BA_events.xlsx')
 
 # %%
-alt.Chart(matrix.reset_index()).mark_line().encode(
-    x = 'date',
-    y = 'Ti'
+#alt.Chart(matrix.reset_index()).mark_line().encode(
+#    x = 'date',
+#    y = 'Ti'
+#)
+
+#%matplotlib widget
+
+fig, ax = plt.subplots()#figsize=(7,7))
+ax.errorbar(
+    x = matrix.index,
+    y = matrix["Ti"],
+    yerr = unc["Ti"],
+    capsize = 2,
+    marker = '.',
+    label = "Ti"
 )
+ax.errorbar(
+    x = matrix.index,
+    y = matrix["Sb"],
+    yerr = unc["Sb"],
+    capsize = 2,
+    label = "Sb"
+)
+ax.legend()
+fig.tight_layout()
+plt.show()
+
+fig, ax = plt.subplots(figsize=(5,5), ncols=2)
+ax[0].set_title = "Soil related"
+sns.boxplot(
+    matrix[["Ti", "Mg", "Al"]],
+    ax = ax[0],
+)
+ax[1].set_title = "Break wear"
+sns.boxplot(
+    matrix[["Sb", "Mo", "Cu"]],
+    ax = ax[1],
+)
+fig.tight_layout()
+plt.show()
 
 #%%
+
 line = alt.Chart(matrix.reset_index()).mark_line().encode(
     x = 'date',
     y = 'Ti'
@@ -47,3 +85,27 @@ ebars = alt.Chart(unc.reset_index()).mark_errorbar().encode(
 line + ebars
 
 #%%
+fig = plt.figure()
+ax = plt.axes()
+
+ax.errorbar(
+    x = matrix.index,
+    y = matrix["Sb"],
+    yerr = unc["Sb"],
+    marker = '.',
+    capsize = 2,
+    capthick = 2,
+)
+
+print(matrix["Sb"].min())
+print(matrix["Sb"].max())
+print(matrix["Sb"].median())
+print(matrix["Sb"].mean())
+#%%
+np.log10(matrix["Sb"]).plot.kde(label="Sb")
+np.log10(matrix["Cu"]).plot.kde(label="Cu")
+np.log10(matrix["PM2.5"]).plot.kde(label="PM$_{2.5}$")
+np.log10(matrix["OC"]).plot.kde(label="OC")
+np.log10(matrix["EC"]).plot.kde(label="EC")
+np.log10(matrix["Na total"]).plot.kde(label="Na total")
+plt.legend()
