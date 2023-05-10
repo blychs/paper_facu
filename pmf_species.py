c7b8e168f7e3a48b7487c216bbc62547ad243134
blycho <pablo.lichtig@gmail.com>
2023-05-10 15:02:45 -0300

    Boxplot added, name changed to pmf_species

--- pmf_species.py
+++ pmf_species.py
@@ -19,7 +19,7 @@ import numpy as np
 import datetime as dt
 import pandas as pd
 #import altair as alt
-#from plotnine import ggplot, geom_smooth, aes, stat_smooth, facet_wrap
+#import plotnine as pn
 from load_data import load_data
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -30,6 +30,9 @@ matrix, unc, meteo, gases, events = load_data('PMF_BA_full.xlsx', 'PMF_BA_full.x
                                                'BA_events.xlsx')
 
 
+unc = unc.add_prefix('unc_')
+matrix_withunc = pd.concat([matrix, unc, events], axis=1)
+#display(matrix_withunc)
 # %%
 #alt.Chart(matrix.reset_index()).mark_line().encode(
 #    x = 'date',
@@ -38,49 +41,79 @@ matrix, unc, meteo, gases, events = load_data('PMF_BA_full.xlsx', 'PMF_BA_full.x
 
 #%matplotlib widget
 
-fig, ax = plt.subplots()#figsize=(7,7))
-ax.errorbar(x = matrix.index, y = matrix["Ti"], yerr = unc["Ti"],
-    capsize = 2, marker = '.', label = "Ti")
-ax.errorbar(x = matrix.index, y = matrix["Sb"], yerr = unc["Sb"],
-    capsize = 2, label = "Sb")
-ax.legend()
-fig.tight_layout()
+p = (
+    pn.ggplot(matrix_withunc.reset_index().rename(columns={"PM2.5": "PM25",
+                                                           "unc_PM2.5": "unc_PM25"})) +
+    #pn.geom_line(pn.aes(x = "date", y = "Ti"), color='b') +
+    #pn.geom_errorbar(pn.aes(x = "date", ymin = "Ti-unc_Ti",
+    #                         ymax = "Ti+unc_Ti", width=4), color='k') +
+    #pn.geom_point(pn.aes(x = "date", y = "Ti"), color='b') +
+    pn.geom_line(pn.aes(x = "date", y = "PM25"), color='k') +
+    pn.geom_point(pn.aes(x = "date", y = "PM25", color="Event")) +
+    pn.geom_errorbar(pn.aes(x = "date", ymin = f"PM25-unc_PM25",
+                             ymax = f"PM25+unc_PM25", width=4, color="Event")) +
+    pn.scale_y_continuous(trans="log10")
+)
+
+p.draw()
 plt.show()
 
-over_gram = lambda x: x / (matrix["PM2.5"] / 1000000)
-fig, ax = plt.subplots(figsize=(7.5,5), ncols=4)
-sns.boxplot(matrix[["Ti", "Mg", "Al"]].apply(over_gram),
-    ax = ax[0])
-ax[0].set_ylabel("µg/g")
-ax[0].set_yscale("log")
-ax[0].set_title("Soil related")
+with plt.style.context('ggplot'):
+    fig, ax = plt.subplots()
+    ax.plot(matrix.index, matrix["PM2.5"], '.-r')
 
-sns.boxplot(matrix[["Sb", "Mo", "Cu"]].apply(over_gram),
-    ax = ax[1])
-ax[1].set_yscale("log")
-ax[1].set_title("Break wear")
-
-sns.boxplot(matrix[["Na sol", "Cl", "Mg"]].apply(over_gram),
-            ax = ax[2])
-ax[2].set_yscale("log")
-ax[2].set_title("Sea Salt")
-
-sns.boxplot(matrix[["OC", "EC"]].apply(over_gram),
-            ax = ax[3])
-ax[3].set_yscale("log")
-ax[3].set_title("Carbonaceous")
-
-fig.tight_layout()
-plt.show()
 
 #%%
-fig = plt.figure()
-ax = plt.axes()
+print(list(matrix.keys()))
+with plt.style.context('ggplot'):
+    fig, ax = plt.subplots()#figsize=(7,7))
+    ax.errorbar(x = matrix.index, y = matrix["Ti"], yerr = unc["unc_Ti"],
+        capsize = 2, marker = '.', label = "Ti")
+    ax.errorbar(x = matrix.index, y = matrix["Sb"], yerr = unc["unc_Sb"],
+        capsize = 2, label = "Sb")
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
+
+#%%
+with plt.style.context('seaborn-v0_8-paper'):
+    over_gram = lambda x: x / (matrix["PM2.5"] / 1000000)
+    fig,ax = plt.subplots(figsize=(7.5,5), ncols=5,
+                           gridspec_kw={"width_ratios":[3,5,3,2,2]})
+    sns.boxplot(matrix[["Ti", "Mg", "Al"]].apply(over_gram),
+        ax = ax[0])
+    ax[0].set_ylabel("µg/g")
+    ax[0].set_yscale("log")
+    ax[0].set_title("Soil related")
+
+    sns.boxplot(matrix[["Sb", "Mo", "Cu", "Pb", "Zn"]].apply(over_gram),
+        ax = ax[1])
+    ax[1].set_yscale("log")
+    ax[1].set_title("Break wear")
+
+    sns.boxplot(matrix[["Na sol", "Cl", "Mg"]].apply(over_gram),
+                ax = ax[2])
+    ax[2].set_yscale("log")
+    ax[2].set_title("Sea Salt")
+
+    sns.boxplot(matrix[["OC", "EC"]].apply(over_gram),
+                ax = ax[3])
+    ax[3].set_yscale("log")
+    ax[3].set_title("Carbonaceous")
+
+    sns.boxplot(matrix[["V", "Zn"]], ax=ax[4])
+    ax[4].set_yscale("log")
+    ax[4].set_title("Exhaust")
+    fig.tight_layout()
+    plt.show()
+
+#%%
+fig, ax = plt.subplots()
 
 ax.errorbar(
     x = matrix.index,
     y = matrix["Sb"],
-    yerr = unc["Sb"],
+    yerr = unc["unc_Sb"],
     marker = '.',
     capsize = 2,
     capthick = 2,
