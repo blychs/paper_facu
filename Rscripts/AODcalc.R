b385d0383fe6b5b2633821ef4521516c197ab042
Melisa Diaz Resquin <melisadr@gmail.com>
2024-04-25 16:57:25 -0300

    Estoy haciendo el PMF_full con el promedio de los blancos. WIP

---
+++ Rscripts/AODcalc.R
@@ -0,0 +1,27 @@
+library(readr)
+library(readxl)
+library(openair)
+# CEILAP_BA_TOT <- read_csv("data/DatosCEILAP/20190101_20200630_CEILAP-BA.tot_lev20", 
+#                           col_types = cols(`Date(dd:mm:yyyy)` = col_datetime(format = "%d:%m:%Y")), 
+#                           skip = 6)
+CEILAP_BA_hora <- read_csv("data/DatosCEILAP/20190101_20200630_CEILAP-BA.lev20", 
+                      col_types = cols(`Date(dd:mm:yyyy)` = col_datetime(format = "%d:%m:%Y")), 
+                      skip = 6, na = "-999")
+CEILAP_BA <- read_csv("data/DatosCEILAP/20190101_20200630_CEILAP-BA.lev20", 
+                      col_types = cols(`Date(dd:mm:yyyy)` = col_datetime(format = "%d:%m:%Y")), 
+                      skip = 6, na ="-999.000000")
+
+CEILAP_BA=CEILAP_BA[,c(1,2,22,65)]
+CEILAP_BA$date = as.POSIXct(paste(CEILAP_BA$`Date(dd:mm:yyyy)`, CEILAP_BA$`Time(hh:mm:ss)`), format="%Y-%m-%d %H:%M:%S",tz='UTC')
+CEILAP_BA$date = CEILAP_BA$date - 12*3600
+CEILAP_BA=timeAverage(CEILAP_BA,avg.time = "day")
+CEILAP_BA = CEILAP_BA[,c(1,4,5)]
+
+BA_events_testM <- read_excel("BA_events_testM.xlsx",
+                              col_types = c("date", "numeric", "numeric",
+                                            "skip", "text"))
+BA_events_testM$date <- as.POSIXct(BA_events_testM$date, tz='UTC')
+BA_events_testM$Event_M <- as.factor(BA_events_testM$Event_M)
+CEILAP_BA = merge(CEILAP_BA, BA_events_testM, by="date")
+CEILAP_BA$diffAOD=CEILAP_BA$AOD_440nm-CEILAP_BA$AOD440
+
