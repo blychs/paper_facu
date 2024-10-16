a9b64446e8dbe241cf51ee29e4293afa56580113
Melisa Diaz Resquin <melisadr@gmail.com>
2024-10-16 17:11:57 -0300

    cambios en la forma de calcular porcentajes en GA

--- Rscripts/graphabstract.R
+++ Rscripts/graphabstract.R
@@ -18,6 +18,13 @@ Simon_mass <- read_csv("../Simon_2011_events_mass.csv",
                        col_types = cols(trace_elements = col_skip()))
 Simon_mass$date <- as.POSIXct(Simon_mass$date, tz='UTC')
 Simon_mass$PM2.5=rowSums(Simon_mass[,2:8])
+Simon_mass$ii_perc=Simon_mass$inorganic_ions/Simon_mass$PM2.5*100
+Simon_mass$om_perc=Simon_mass$organic_mass/Simon_mass$PM2.5*100
+Simon_mass$gm_perc=Simon_mass$geological_minerals/Simon_mass$PM2.5*100
+Simon_mass$ec_perc=Simon_mass$elemental_C/Simon_mass$PM2.5*100
+Simon_mass$ss_perc=Simon_mass$salt/Simon_mass$PM2.5*100
+Simon_mass$others_perc=Simon_mass$others/Simon_mass$PM2.5*100
+Simon_mass$r_perc=Simon_mass$residual/Simon_mass$PM2.5*100
 
 BA_events_testM <- read_excel("../BA_events_testMnew2.xlsx")
 BA_events_testM$date <- as.POSIXct(BA_events_testM$date, tz='UTC')
@@ -47,17 +54,17 @@ PMF_BA_full$date2 <- as.Date(PMF_BA_full$date, format = "%Y-%m-%d")
 # Simon_mass$residual[Simon_mass$residual<0]=0
 mean_data <- Simon_mass %>%
   mutate(Event_F = PMF_BA_full$color) %>%
-  summarise(across(c(inorganic_ions, organic_mass, elemental_C, geological_minerals, salt, others, residual),\(x) mean(x, na.rm = TRUE))) %>%
-  pivot_longer(cols = c(inorganic_ions, organic_mass, elemental_C, geological_minerals, salt, others,residual),
+  summarise(across(c(ii_perc, om_perc, ec_perc, gm_perc, ss_perc, others_perc, r_perc),\(x) mean(x, na.rm = TRUE))) %>%
+  pivot_longer(cols = c(ii_perc, om_perc, ec_perc, gm_perc, ss_perc, others_perc, r_perc),
                names_to = "category", values_to = "value") %>%
   mutate(category = recode(category,
-                           "inorganic_ions" = "Inorganic Ions \n 10%",
-                           "organic_mass" = "Organic Matter \n 58%",
-                           "elemental_C" = "Elemental Carbon \n 6%",
-                           "geological_minerals" = "Geological Minerals \n 14%",
-                           "salt" = "Sea Salt \n 3%",
-                           "others" = "KNON \n 2%",
-                           "residual" ="Others \n 7%"))
+                           "ii_perc" = "Inorganic Ions \n 10%",
+                           "om_perc" = "Organic Matter \n 57%",
+                           "ec_perc" = "Elemental Carbon \n 6%",
+                           "gm_perc" = "Geological Minerals \n 14%",
+                           "ss_perc" = "Sea Salt \n 3%",
+                           "others_perc" = "KNON \n 2%",
+                           "r_perc" = "Others \n 9%"))
 
 # Crear gráfico de barras de PM2.5 ####
 bar_plot <- ggplot(PMF_BA_full, aes(x = date2, y = `PM2,5`, fill = color)) +
@@ -72,7 +79,7 @@ bar_plot
 # Create average pie chart 
 mean_data <-mean_data %>% 
   arrange(desc(category)) %>%
-  mutate(percentage = value / sum(value) * 100,
+  mutate(percentage = value ,
          ypos = cumsum(value) - 0.5* value)
 
 average_pie <- ggplot(data= mean_data, aes(x = "", y = value, fill = category)) +
